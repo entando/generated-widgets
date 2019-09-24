bc586fa2cbff175400dfd98be46fb913f6ae685c
Martynas Rimšelis <rimselis@gmail.com>
2019-09-24 11:53:42 +0300

    removed unused mock file, moved files

--- react/details/src/components/__tests__/Widget.test.js
+++ react/details/src/components/__tests__/Widget.test.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import Widget from 'components/Widget';
-import MockEntity from 'assets/mock-entity.json';
 
 import 'services/i18n/i18n';
 
 test('renders widget', async () => {
-  const { getByTestId } = render(<Widget entity={MockEntity} />);
+  const { getByTestId } = render(<Widget />);
 
   expect(getByTestId('widget-name-heading'));
 });
