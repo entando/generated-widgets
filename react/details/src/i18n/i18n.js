6426bb540c749717e11ff8104119ccadcb50a18a
Martynas Rimšelis <rimselis@gmail.com>
2019-09-30 11:04:57 +0300

    removed trnaslation field from locales

--- react/details/src/i18n/i18n.js
+++ react/details/src/i18n/i18n.js
@@ -3,9 +3,16 @@ import { initReactI18next } from 'react-i18next';
 
 import locales from 'i18n/locales';
 
+function getResources(translations) {
+  return Object.keys(translations).reduce(
+    (acc, lang) => ({ ...acc, [lang]: { translation: translations[lang] } }),
+    {}
+  );
+}
+
 i18next.use(initReactI18next).init({
   keySeparator: '.',
-  resources: locales,
+  resources: getResources(locales),
   interpolation: {
     escapeValue: false,
   },
