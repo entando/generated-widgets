cdf3287a9e157f0c40c266430f7f730bc798a7af
Martynas Rimšelis <rimselis@gmail.com>
2019-09-30 14:52:11 +0300

    reverted translation init

--- react/details/src/i18n/i18n.js
+++ react/details/src/i18n/i18n.js
@@ -3,18 +3,9 @@ import { initReactI18next } from 'react-i18next';
 
 import locales from 'i18n/locales';
 
-// function getResources(translations) {
-//   return Object.keys(translations).reduce(
-//     (acc, lang) => ({ ...acc, [lang]: { translation: translations[lang] } }),
-//     {}
-//   );
-// }
-
 i18next.use(initReactI18next).init({
   keySeparator: '.',
   resources: locales,
-  ns: Object.keys(locales[Object.keys(locales)[0]]),
-  nsSeparator: '.',
   interpolation: {
     escapeValue: false,
   },
