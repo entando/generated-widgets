05abeb8e8cc106a42147f7497810c1c3b4f5f004
Martynas Rimšelis <rimselis@gmail.com>
2019-12-08 18:52:46 +0200

    addded concurent run of bundles with build

--- wrapper/build-form.sh
+++ wrapper/build-form.sh
@@ -1,12 +1,19 @@
 pushd ../react/form
 
+if [ ! -e ".env" ]; then
+  echo "Adding .env file"
+  echo "REACT_APP_DOMAIN=http://localhost:8081/services/jhipster/api" >> ".env"
+fi
+
+npm install
+
 INLINE_RUNTIME_CHUNK=false npm run build
 
 pushd build/static/js
 
-mv -f 2*.js vendor.conference-form.js
-mv -f main*.js main.conference-form.js
-mv -f runtime~main*.js runtime.conference-form.js
+cp -f 2*.js vendor.conference-form.js
+cp -f main*.js main.conference-form.js
+cp -f runtime~main*.js runtime.conference-form.js
 
 popd
 
