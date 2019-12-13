4779bb37b1c032fcb6a51e2354984a63415d3771
Giuseppe de Santis <g.desantis@entando.com>
2019-12-13 02:20:43 +0100

    ENG-74 remove unneeded code

--- react/table/src/components/pagination/PaginationWrapper.js
+++ react/table/src/components/pagination/PaginationWrapper.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { withTranslation } from 'react-i18next';
